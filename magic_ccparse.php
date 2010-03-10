ca418bb8bea0d43a25902e020f9d85800a34da6a
Mark Harviston <infinull@gmail.com>
2010-03-09 23:07:17 -0800

    added boilerplate

--- magic_ccparse.php
+++ magic_ccparse.php
@@ -1,4 +1,23 @@
 <?php
+/** Author: Mark Harviston
+ *  Purpose: normalize (sort, reduce) Magic: The Gathering color codes
+ *
+ *  (c) 2010 Mark Harviston <infinull@gmail.com> Some Rights Reserved
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #color wheel node
 class cwn {
@@ -24,7 +43,6 @@ function ccparse($str='', $html=false){
 	//must contain only digit, space, color code or {}, X or T(tap)
 	//X and/or Tap must not be w/i {}
 	//There must be only one nested level of {}
-	//
 
 	$colorless = 0;
 	$colors = array();
